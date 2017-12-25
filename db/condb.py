8c7aa7a5d4b9a45fbfc1b21296310d3dc37268fe
wsj31013 <travis31013@naver.com>
2017-12-25 20:00:35 +0900

    Modified db connection file

--- db/condb.py
+++ db/condb.py
@@ -1,8 +1,9 @@
 import pymysql
 
-conn = pymysql.connect(host="127.0.0.1", user="root", password="dntjdwls12", db="scraping")
-cur = conn.cursor()
-cur.execute("select * from pages where id=2")
-print(cur.fetchone())
-cur.close()
-conn.close()
+conn = pymysql.connect(host="127.0.0.1", user="root", password="dntjdwls12", db="mysql", charset="utf8")
+
+# cur = conn.cursor()
+# cur.execute("select * from pages where id=2")
+# print(cur.fetchone())
+# cur.close()
+# conn.close()
