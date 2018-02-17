0bf7735c638b4d55e60b22cfacc261aab4f8bd61
wsj31013 <travis31013@naver.com>
2018-02-17 14:21:48 +0900

    Modified bestseller crawller

--- kyobobest/bestseller.py
+++ kyobobest/bestseller.py
@@ -11,8 +11,8 @@ book_page_urls = [a.attrs.get('href') for a in bsObj.select('div.title a[href^="
 
 # print(book_page_urls)
 for book_page_url in book_page_urls:
-    response = requests.get(book_page_url)
-    bsObj = BeautifulSoup(response.text, "html.parser")
+    req = requests.get(book_page_url)
+    bsObj = BeautifulSoup(req.text, "html.parser")
     title = bsObj.select( "h1.title strong" )[0].get_text().strip()
     author = bsObj.select( "span.name  a" )[0].get_text()
     print (title + " / " + "저자:" + author)
