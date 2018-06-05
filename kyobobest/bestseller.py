5bbcc97c380b1889d3fb5f8ba6cb4c496131df48
wsj31013 <travis31013@naver.com>
2018-06-05 16:48:20 +0900

    Modified File Store and with

--- kyobobest/bestseller.py
+++ kyobobest/bestseller.py
@@ -10,11 +10,17 @@ html = req.text
 bsObj = BeautifulSoup(html, "html.parser")
 book_page_urls = [a.attrs.get('href') for a in bsObj.select('div.title a[href^="http://www.kyobobook.co.kr/product/detailViewKor.laf"]')]
 
-f = open("result.txt","w")
+
+f = open("result.txt","a", encoding="utf-8")
+
+
 # print(book_page_urls)
-for book_page_url in book_page_urls:
-    req = requests.get(book_page_url)
-    bsObj = BeautifulSoup(req.text, "html.parser")
-    title = bsObj.select( "h1.title strong" )[0].get_text().strip()
-    author = bsObj.select( "span.name  a" )[0].get_text()
-    print (title + " / " + "저자:" + author)
+with open("result.txt", "a", encoding="utf-8") as f:
+    for book_page_url in book_page_urls:
+        req = requests.get(book_page_url)
+        bsObj = BeautifulSoup(req.text, "html.parser")
+        title = bsObj.select( "h1.title strong" )[0].get_text().strip()
+        author = bsObj.select( "span.name  a" )[0].get_text()
+        # print (title + " / " + "저자:" + author)
+        data = (title + " / " + "저자:" + author)
+        f.write(data)
